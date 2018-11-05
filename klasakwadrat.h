59d40ed292d8975eb1b3216b915df98e13bbfacc
Tomaszuniek <gorka11@interia.eu>
2018-11-05 23:38:09 +0100

    drugi

---
+++ klasakwadrat.h
@@ -0,0 +1,23 @@
+
+class Kwadrat{
+
+int a;
+int b;
+int c;
+
+
+Kwadrat(int aa, int bb, int cc){
+}
+
+Kwadrat(){
+
+}
+
+
+double miejscezerowe(unsigned int ktore, myexception myex){
+
+double delta;
+double m1;
+double m2;
+}
+};
